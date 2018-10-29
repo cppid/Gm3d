3f807fafb4666087c7938c894acdc7c8cb4889cb
Januar Andaria <janucaria@gmail.com>
2018-10-29 23:38:32 +0800

    Add full float template specialization for Rgba

--- src/gm3d/Rgba.hpp
+++ src/gm3d/Rgba.hpp
@@ -2,6 +2,7 @@
 #define GM3D_RGBA_HPP
 
 #include <cstdint>
+#include <type_traits>
 
 #include "detail/BasicRgba.hpp"
 
@@ -22,6 +23,38 @@ public:
 
   Rgba(Scalar r, Scalar g, Scalar b) noexcept
   : Rgba{r, g, b, 0xFF}
+  {
+  }
+};
+
+template<>
+class Rgba<float> : public detail::BasicRgba<Rgba<float>, float> {
+public:
+  using Scalar = detail::BasicRgba<Rgba<float>, float>::Scalar;
+
+  using detail::BasicRgba<Rgba<float>, float>::BasicRgba;
+
+  Rgba() noexcept = default;
+
+  template<typename T,
+           typename = std::enable_if_t<std::is_same_v<T, std::uint8_t>>>
+  explicit Rgba(Rgba<T>&& col)
+  : Rgba{col.r(), col.g(), col.b(), col.a()}
+  {
+  }
+
+  template<typename T,
+           typename = std::enable_if_t<std::is_same_v<T, std::uint8_t>>>
+  Rgba(T r, T g, T b, T a) noexcept
+  : Rgba{r / Scalar{255} * a / Scalar{255},
+         g / Scalar{255} * a / Scalar{255},
+         b / Scalar{255} * a / Scalar{255},
+         a / Scalar{255}}
+  {
+  }
+
+  Rgba(Scalar r, Scalar g, Scalar b) noexcept
+  : Rgba{r, g, b, 1}
   {
   }
 };
